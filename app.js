3a3c3073c1283f3e41ba1216c1f0729231757486
Zaid khan <160598429+Zaidkhan009@users.noreply.github.com>
2024-03-19 01:55:22 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+"use strict";
+Exercise;
+13;
+let transportation = [`civic`, `bike`, `bus`, `suzuki  jisme cow ho `];
+for (let i = 0; i < transportation.length; i++) {
+    console.log(`i would  like to own a ` + transportation[i]);
+}
